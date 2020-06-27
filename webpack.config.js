4b396dc2da4f766553065763a5b17ba4cde2cb22
Gang Zhou <jasonzhou@Gangs-MacBook-Pro.local>
2020-06-27 19:57:58 +0800

    advanced webpack configuration - 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -20,7 +22,12 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled Javascipt Project',
+      template: './src/index.html'
+    })
   ],
   devServer: {
     contentBase: './dist',
