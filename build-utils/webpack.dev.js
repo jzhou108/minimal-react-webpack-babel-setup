2a4f901f91095d3d99652c22e459d722f9fac195
Gang Zhou <jasonzhou@Gangs-MacBook-Pro.local>
2020-07-04 20:14:06 +0800

    add .env files, if really use should exclude in .gitignore

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,9 +1,13 @@
+const Dotenv = require('dotenv-webpack');
 const { DefinePlugin } = require('webpack');
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   plugins: [
+    new Dotenv({
+      path: './.env.development',
+    }),
     new DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('development'),
--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,9 +1,13 @@
+const Dotenv = require('dotenv-webpack');
 const { DefinePlugin } = require('webpack');
 
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
+    new Dotenv({
+      path: './.env.production',
+    }),
     new DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
